ad94e514fd2b372975ab957dbf931d5045bcd9d5
John Fellman <john.fellman@avrs.com>
2014-08-09 10:53:31 -0700

    same

--- app/uploaders/cover_photo_uploader.rb
+++ app/uploaders/cover_photo_uploader.rb
@@ -34,7 +34,7 @@ class CoverPhotoUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id + 1}"
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
