1b02521a0aff3e6e5f61616bcb0d71366652459c
John Fellman <john.fellman@avrs.com>
2015-05-09 15:12:39 -0700

    also orders splits

--- app/assets/javascripts/collections/splits.js
+++ app/assets/javascripts/collections/splits.js
@@ -1,3 +1,7 @@
 Ets.Collections.splits = Backbone.Collection.extend({
+    comparator: 'order',
+    initialize: function () {
+        this.listenTo(this, 'sync add change', this.sort);
+    },
     model: Ets.Models.splits
 });
